b89dd034c0d3f299f4271b748ec4e7c7f0adbf99
VitalR <vitalii.rodikov@gmail.com>
2022-12-02 11:50:54 +0200

    Add more liquidity

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -12,46 +12,51 @@ contract Exchange {
         token = _token;
     }
 
-    function addLiquidity(uint256 _amount) public payable {
-        IERC20(token).transferFrom(msg.sender, address(this), _amount);
+    function addLiquidity(uint tokenAmount) public payable {
+        if (getReserve() == 0) {
+            IERC20(token).transferFrom(msg.sender, address(this), tokenAmount);
+        } else {
+            uint ethReserve = address(this).balance;
+            uint tokenReserve = getReserve();
+            uint tokenAmount_ = (msg.value * tokenReserve) / ethReserve;
+            require(tokenAmount >= tokenAmount_, "Insufficient token amount");
+
+            IERC20(token).transferFrom(msg.sender, address(this), tokenAmount_);
+        }
     }
 
-    function getReserve() public view returns (uint256) {
+    function getReserve() public view returns (uint) {
         return IERC20(token).balanceOf(address(this));
     }
 
-    function getPrice(uint256 inputReserve, uint256 outputReserve)
-        public
-        view
-        returns (uint256)
-    {
+    function getPrice(uint inputReserve, uint outputReserve) public pure returns (uint) {
         require(inputReserve > 0 && outputReserve > 0, "Invalid reserves");
 
         return (inputReserve * 1000) / outputReserve;
     }
 
     function getOutputAmount(
-        uint256 inputAmount,
-        uint256 inputReserve,
-        uint256 outputReserve
-    ) private pure returns (uint256) {
+        uint inputAmount,
+        uint inputReserve,
+        uint outputReserve
+    ) private pure returns (uint) {
         require(inputReserve > 0 && outputReserve > 0, "Invalid reserves");
 
         return (outputReserve * inputAmount) / (inputReserve + inputAmount);
     }
 
-    function getTokenAmount(uint256 etherSold) public view returns (uint256) {
+    function getTokenAmount(uint etherSold) public view returns (uint) {
         require(etherSold > 0, "etherSold is too small");
 
-        uint256 tokenReserve = getReserve();
+        uint tokenReserve = getReserve();
 
         return getOutputAmount(etherSold, address(this).balance, tokenReserve);
     }
 
-    function getEtherAmount(uint256 tokenSold) public view returns (uint256) {
+    function getEtherAmount(uint tokenSold) public view returns (uint) {
         require(tokenSold > 0, "tokenSold is too small");
 
-        uint256 tokenReserve = getReserve();
+        uint tokenReserve = getReserve();
 
         return getOutputAmount(tokenSold, tokenReserve, address(this).balance);
     }
