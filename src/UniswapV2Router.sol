2fe71cf7b4cec50c37c0b969f67493f72be50210
VitalR <vitalii.rodikov@gmail.com>
2022-12-17 12:10:07 +0200

    Started with Router

---
+++ src/UniswapV2Router.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "./UniswapV2Factory.sol";
+
+interface IUniswapV2Factory {
+    function createPair(address, address) external returns (address);
+    function pairs(address, address) external returns (address);
+}
+
+error InsufficientAAmount();
+error InsufficientBAmount();
+error SafeTransferFailed();
+
+contract UniswapV2Router {
+
+    IUniswapV2Factory factory;
+
+    constructor(address factory_) {
+        factory = IUniswapV2Factory(factory_);
+    }
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBmin,
+        address to
+    ) public returns (uint amountA, uint amountB, uint liquidity) {
+
+        if (factory.pairs(tokenA, tokenB) == address(0)) {
+            factory.createPair(tokenA, tokenB);
+        }
+
+        (amountA, amountB) = _calculateLiquidity(
+            tokenA,
+            tokenB,
+            amountADesired,
+            amountBDesired,
+            amountAMin,
+            amountBmin
+        );
+
+        address pair = UniswapV2Library.pairFor(
+            address(factory),
+            tokenA,
+            tokenB
+        );
+
+        _safeTransferFrom(tokenA, msg.sender, pair, amountA);
+        _safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        liquidity = IUniswapV2Pair(pair).mint(to);
+    }
+
+
+    function _caulculateLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin
+    ) internal returns (uint amountA, uint amountB) {
+        
+        (uint reserveA, uint reserveB) = UniswapV2Library.getReserves(
+            address(factory),
+            tokenA,
+            tokenB
+        );
+
+        if (reserveA == 0 && reserveB == 0) {
+            (amountA, amountB) = (amountADesired, amountBDesired);
+        } else {
+            uint amountBOptimal = UniswapV2Library.quote(
+                amountADesired,
+                reserveA,
+                reserveB
+            );
+            if (amountBOptimal <= amountBDesired) {
+                if (amountBOptimal <= amountBMin) revert InsufficientBAmount();
+                (amountA, amountB) = (amountADesired, amountBDesired);
+            } else {
+                uint amountAOptimal = UniswapV2Library.quote(
+                    amountBDesired,
+                    reserveB,
+                    reserveA
+                );
+                assert(amountAOptimal <= amountADesired);
+
+                if (amountAOptimal <= amountAMin) revert InsufficientAAmount();
+                (amountA, amountB) = (amountAOptimal, amountBDesired);
+            }
+        }
+
+
+    }
+
+
+}
